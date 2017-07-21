7f127afb7c3b1c7be5e8828750196c08c0be3959
Kolen Cheung <christian.kolen@gmail.com>
2017-07-21 14:37:28 -0700

    setup.py: improve syntax in 84d59bb

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 # Import version number
 version = {}
-with open("yaml2cli" + path.sep + "version.py") as f:
+with open(path.join("yaml2cli", "version.py")) as f:
     exec(f.read(), version)
 version = version['__version__']
 
