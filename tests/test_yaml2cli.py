26ecd620a419da6bc131fbea1c89e026323eac72
Kolen Cheung <christian.kolen@gmail.com>
2017-07-21 13:26:00 -0700

    add more tests

--- tests/test_yaml2cli.py
+++ tests/test_yaml2cli.py
@@ -1,6 +1,6 @@
 """
 """
-from .context import option2arg
+from .context import option2arg, dict2command
 from collections import OrderedDict
 
 
@@ -11,3 +11,14 @@ def test_option2arg():
                           ('flag', None)])
     assert option2arg(option, None, None, 'cori') == [
         '-n 3 --test case1 case2 -c 10 --flag']
+
+
+def test_dict2command():
+    arg_dict = OrderedDict([('command', 'my_program'),
+                            ('option',
+                             [OrderedDict([('c', OrderedDict([('cori', 10), ('gordita', 1)])),
+                                           ('flag', None),
+                                           ('n', 3),
+                                           ('test', ['case1', 'case2'])])])])
+    assert dict2command(arg_dict) == [
+        'my_program -c 10 --flag -n 3 --test case1 case2']
